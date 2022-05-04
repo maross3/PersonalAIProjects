a693d25530db36dcb189ad193a3cc26cb1365cb6
maross3 <maross334@gmail.com>
2022-05-04 11:50:12 -0400

    Update BaseFunctions.js

--- ScreepsWorld/BaseFunctions.js
+++ ScreepsWorld/BaseFunctions.js
@@ -1,8 +1,9 @@
-/* global ERR_NOT_IN_RANGE RESOURCE_ENERGY, WORK, CARRY, MOVE */
+/* global ERR_NOT_IN_RANGE RESOURCE_ENERGY, WORK, CARRY, MOVE, FIND_STRUCTURES
+  FIND_MY_CONSTRUCTION_SITES */
 
 var baseWorker = {
   creep: 'none',
-  body: [WORK, WORK, WORK, CARRY, MOVE],
+  body: [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE],
   act: fillSpawn
 }
 
@@ -10,24 +11,35 @@ var stationaryWorker = {
   body: [WORK, WORK, WORK, WORK, CARRY, MOVE]
 }
 
-module.exports = { buildRoads, keepExtenFull, fillSpawn, baseWorker, repairStructure, stationaryWorker }
-
-function repairStructure (creep, source, structure) {
-  if (!creep) return
-  setCreepMemory(creep)
-
-  if (!creep.memory.filling) {
-    if (creep.harvest(source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) creep.moveTo(source)
-  } else if (creep.repair(structure) === ERR_NOT_IN_RANGE) creep.moveTo(structure)
+module.exports = {
+  keepExtenFull,
+  fillSpawn,
+  baseWorker,
+  stationaryWorker,
+  repairStructures,
+  buildAllConstructionSites
 }
 
-function buildRoads (creep, road, source) {
+function buildAllConstructionSites (creep, source) {
   if (!creep) return
   setCreepMemory(creep)
+  var constSites = findMyConstSites(creep)
 
-  if (!creep.memory.filling) {
-    if (creep.harvest(source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) creep.moveTo(source)
-  } else if (creep.build(road) === ERR_NOT_IN_RANGE) creep.moveTo(road)
+  if (creep.memory.filling) {
+    if (!constSites[0]) creep.say('zZzZz')
+    else if (creep.build(constSites[0]) === ERR_NOT_IN_RANGE) creep.moveTo(constSites[0])
+  } else {
+    if (creep.harvest(source) === ERR_NOT_IN_RANGE) creep.moveTo(source)
+  }
+}
+
+function findMyConstSites (creep) {
+  var constSites = creep.room.find(FIND_MY_CONSTRUCTION_SITES, {
+    filter: (cs) => {
+      return (cs)
+    }
+  })
+  return constSites
 }
 
 function keepExtenFull (creep, source, extensions, contrl) {
@@ -61,4 +73,22 @@ function fillSpawn (creep, source, spawn) {
   if (!creep.memory.filling) {
     if (creep.harvest(source) === ERR_NOT_IN_RANGE) creep.moveTo(source)
   } else if (creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) creep.moveTo(spawn)
+}
+
+function repairStructures (creep, source) {
+  if (!creep) return
+  setCreepMemory(creep)
+
+  var structure = creep.room.find(FIND_STRUCTURES, {
+    filter: (str) => {
+      return (str.hits < str.hitsMax && str.pos.y > 2 && str.hitsMax < 500000)
+    }
+  })
+
+  if (creep.memory.filling) {
+    if (!structure[0]) creep.say('zZzZz')
+    else if (creep.repair(structure[0]) === ERR_NOT_IN_RANGE) creep.moveTo(structure[0])
+  } else {
+    if (creep.harvest(source) === ERR_NOT_IN_RANGE) creep.moveTo(source)
+  }
 }
